ee0813f90d050d1d4f52834e27007c8e720b9d21
Angel <alu0101139456@ull.edu.es>
2019-10-22 19:37:53 +0100

    Terminada la representación de la concatenación en el string

--- P5/src/expreg_t.cpp
+++ P5/src/expreg_t.cpp
@@ -16,9 +16,9 @@ alfa_(alfa),
 formato_(formato){
   if (alfa_.is_in_alphabet(expre_)) {  //Se comprueba que todos los caracteres
                                        //pertenecen al alfabeto
-    expreg_t posfija = convert_to_posfija(1);
-    //convert();
-    std::cout << ex_pf_ << " salio....\n";
+    //expreg_t posfija = convert_to_posfija(1);
+    convert();
+    //std::cout << ex_pf_ << " salio....\n";
   }
   else
     std::cerr << "Hay caracteres que no pertenecen al alfabeto establecido\n";
@@ -33,11 +33,11 @@ expreg_t expreg_t::convert_to_posfija(unsigned formato) {
     if(aux.is_ParAb()) {
       pila_.push(aux);
     }
-    if(aux.is_operando) {
+    if(aux.is_operando()) {
       ex_pf_.push_back(aux.get_caracter());
     }
     else if(pila_.empty() || pila_.top().is_ParAb()) {
-      pila_.push(aux.get_caracter());
+      pila_.push(aux);
     }
     else if(aux.is_ParCe()) {
       while (!pila_.top().is_ParAb()) {
@@ -54,24 +54,45 @@ expreg_t expreg_t::convert_to_posfija(unsigned formato) {
 void expreg_t::convert() {
   std::string string_final;
   caracter_t previo = alfa_.find(expre_[0]);
-  string_final = string_final.append(1,expre_[0]);
+  string_final.push_back(expre_[0]);
   for(size_t i=1; i < expre_.size(); i++) {
     caracter_t aux = alfa_.find(expre_[i]);
     if( previo.is_operando() && aux.is_operando() ){
-      string_final = string_final.append(1, '&');
-      string_final = string_final.append(1, aux.get_caracter());
-      previo = aux;
+      string_final.push_back('&');
+      string_final.push_back(aux.get_caracter());
     }
-    else
-      string_final = string_final.append(1, expre_[i]);
-      previo = aux;
-  }
-  if ( string_final[string_final.size()-1] == '*' )
-    string_final = string_final.append(1, '&');
+    else if( previo.is_ParAb() && aux.is_operando()) {
+      string_final.push_back(aux.get_caracter());
+    }
+    else if( previo.is_operando() && aux.is_operador() &&
+        previo.get_caracter() != '*') {
 
+      string_final.push_back(aux.get_caracter());
+    }
+    else if( previo.is_operador() && aux.is_operando() &&
+        previo.get_caracter() != '*'){
+      string_final.push_back(aux.get_caracter());
+    }
+    else if( previo.is_operando() && aux.is_ParCe()) {
+      string_final.push_back(aux.get_caracter());
+    }
+    else if( previo.is_ParCe() && aux.get_caracter() == '*') {
+      string_final.push_back(aux.get_caracter());
+    }
+    else if((previo.get_caracter() == '*') &&  (aux.is_operando()) ) {
+      string_final.push_back('&');
+      string_final.push_back(aux.get_caracter());
+    }
+    else if( previo.is_ParCe() && aux.is_ParAb() ) {
+      string_final.push_back('&');
+      string_final.push_back(aux.get_caracter());
+    }
+    previo = aux;
+  }
   //std::cout << "String de entrada: " << expre_ << "\nString de salida: " <<
   //  string_final << std::endl;
 }
+
 
 
 
