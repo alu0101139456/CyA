387e69ef4912f688f3796353053207e74a3bf680
Angel <alu0101139456@ull.edu.es>
2019-11-02 20:42:36 +0000

    Update

--- P7/NFA2DFA_t.hpp
+++ P7/NFA2DFA_t.hpp
@@ -7,3 +7,27 @@
  * Práctica 7: Construcción de Subconjuntos                                   *
  ****************************************************************************/ 
 //                                NFA2DFA_T.hpp
+
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include "dfa_t.hpp"
+#include "nfa_t.hpp"
+#include "estado_t.hpp"
+:x
+
+class nfa2dfa_t {
+ private:
+
+
+
+ public:
+  void read_file();
+
+ private:
+  void read_alpha_from_file();
+  void read_states_from_file();
+  void read_acept_states_from_file();
+  void read_transitions_from_file();
+};
