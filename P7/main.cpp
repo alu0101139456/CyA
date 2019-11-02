9e5bcceb19e6dacc6904296a09f60c2f9a42eff2
Angel <alu0101139456@ull.edu.es>
2019-11-02 20:01:15 +0000

    Creado main con opción de --help y fichero help de lectura

---
+++ P7/main.cpp
@@ -0,0 +1,45 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++¶
+ * Autor: Ángel Julián Bolaño Campos                                          *
+ * Asignatura: Computabilidad y Algoritmia                                    *
+ * Curso: 2º Ingeniería Informática                                           *
+ * Editor: VIM                                                                *
+ * Estilo Google C++ Style Guide                                              *
+ * Práctica 7: Construcción de Subconjuntos                                   *
+ ****************************************************************************/
+//                                main.cpp
+
+#include<iostream>
+#include<fstream>
+
+void help();
+
+int main (int argc, char* argv[]) {
+
+  if (argc > 1) {
+    std::string a = argv[1];
+    if(a == "--help") {
+      help();
+    }
+    else if(argc == 3) {
+      std::cout << "Numero valido de argumentos\n" << std::endl;
+    }
+    else std::cout << "Numero de argumentos invalido";
+  }
+  else {
+    std::cout << "Modo de empleo: ./NFA2DFA input.nfa output.dfa\n";
+    std::cout << "Pruebe ’NFA2DFA --help’ para más información. \n";
+  }
+}
+
+
+
+void help() {
+  system("clear");
+  std::ifstream in("help");
+  std::string out;
+  while(!in.eof()) {
+    getline(in, out);
+    std::cout << out << std::endl;
+  }
+  
+}
