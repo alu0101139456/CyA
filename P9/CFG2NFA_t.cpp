16e475493a1cdfb6451dd7b7cde4f0f5fe0d8373
Angel <alu0101139456@ull.edu.es>
2019-11-16 17:29:24 +0000

    remove

--- P9/CFG2NFA_t.cpp
+++
@@ -1,8 +0,0 @@
-#include "CFG2NFA_t.hpp"
-
-CFG2NFA_t::CFG2NFA_t(std::string infile, std::string outfile): cfg_(infile){}
-
-
-
-void CFG2NFA_t::print_grammar(){ cfg_.convert_to_nfa(); cfg_.print();}
-void CFG2NFA_t::print_autommata(){}
